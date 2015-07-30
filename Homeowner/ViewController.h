ee337db0b800c82e2f646c90ea9ec6baff96679e
Josh <Joshkmotley@gmail.com>
2015-07-30 15:24:25 -0400

    Initial Commit

---
+++ Homeowner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homeowner
+//
+//  Created by Joshua Motley on 7/30/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Homeowner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Homeowner
+//
+//  Created by Joshua Motley on 7/30/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
